ade21235fd207520a46e0c6cecbf3bb9bd3dd947
MattiaMarseglia1 <m.marseglia1@studenti.unisa.it>
2025-06-24 07:44:07 +0000

    ADD: CBAM module MOD: yaml files

--- CVAE/VAE.py
+++ CVAE/VAE.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import os
 import torch.nn.functional as F
+from cbam import CBAM
+import cv2
 
 class VAutoEncoder(nn.Module):
     "Assumes that the input images are 64x64"
@@ -43,14 +45,15 @@ class VAutoEncoder(nn.Module):
         z = torch.randn(size=(self.latent_size,))
         with torch.no_grad():
             image_generated = self.decoder(torch.cat([z.unsqueeze(0).to(device),y.unsqueeze(0).to(device)], dim=1))
+            
         
-        plt.figure()
-        plt.imshow(image_generated[0].cpu().permute(1,2,0))     # CxHxW -> HxWxC
-        plt.axis('off')
         label_str = '_'.join(str(int(v)) for v in y.tolist())
         os.makedirs(save_folder, exist_ok=True)
-        plt.savefig(os.path.join(save_folder, label_str))
 
+        img = (image_generated.squeeze().permute(1, 2, 0).cpu().numpy() * 255).astype('uint8')
+        img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(os.path.join(save_folder, f'generated_{label_str}.png'), img_bgr)
+        
 
     def train_one_epoch(self, criterion, optimizer, train_loader, device):
         average_loss = 0.0
@@ -123,11 +126,10 @@ class ResidualBlock(nn.Module):
         if out_channels is None:
             out_channels = in_channels
 
-        self.conv1 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
-        
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
         self.skip = nn.Sequential()
@@ -147,77 +149,96 @@ class Res_VAE(VAutoEncoder):
     def __init__(self, architecture_yaml):
 
         self.latent_size = architecture_yaml['LATENT_SIZE']
-        
         self.img_channels=3 
-        self.cond_dim=3
+        
+        self.cond_dim= self.latent_size // 4  # Dimensione della condizione
         super().__init__(self.latent_size)
+        self.cond_latent_proj = nn.Linear(3, self.cond_dim)  # Proiezione della condizione
 
     def build_encoder(self):
         return nn.Sequential(
-            nn.Conv2d(self.img_channels, 64, kernel_size=3, stride=2, padding=1),  # 64x32x32
+            nn.Conv2d(self.img_channels + 3, 64, kernel_size=3, stride=2, padding=1),  # 64x32x32
             nn.ReLU(),
             ResidualBlock(64, 128, stride=2),  # 128x16x16
+            ResidualBlock(128, 128, stride=1), # 128x16x16
+            CBAM(128),  # Attention module
             ResidualBlock(128, 256, stride=2),  # 256x8x8
+            ResidualBlock(256, 256, stride=1),  # 256x8x8
+            CBAM(256),  # Attention module
             ResidualBlock(256, 512, stride=2),  # 512x4x4
+            ResidualBlock(512, 512, stride=1),  # 512x4x4
+            CBAM(512),  # Attention module
             nn.AdaptiveAvgPool2d((1, 1)),
             nn.Flatten(),
             nn.Linear(512, 2*self.latent_size)
         )
 
     def build_decoder(self):
+        class DecoderBlock(nn.Module):
+            def __init__(self, in_channels, out_channels):
+                super().__init__()
+                self.block = nn.Sequential(
+                    ResidualBlock(in_channels, in_channels),
+                    ResidualBlock(in_channels, in_channels),
+                    CBAM(in_channels),
+                    nn.ConvTranspose2d(in_channels, out_channels, 3, stride=2, padding=1, output_padding=1),
+                    nn.BatchNorm2d(out_channels),
+                    nn.ReLU()
+                )
+            def forward(self, x):
+                return self.block(x)
+
         model = nn.Sequential()
         size = 4
-        prev = 256
+        prev = 512  # aumentato da 256 a 512
 
-        # Fully connected and reshape
-        model.append(nn.Linear(self.latent_size + 3, prev * size * size))
+        model.append(nn.Linear(self.latent_size + self.cond_dim, prev * size * size))
         model.append(nn.ReLU())
         model.append(nn.Unflatten(1, (prev, size, size)))
 
-        channels = [256, 128, 64, 32]  # Progressive decoding
-        for k in channels:
-            model.append(ResidualBlock(prev, prev))
-            model.append(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False))
-            model.append(nn.Conv2d(prev, k, kernel_size=3, padding=1))
-            model.append(nn.BatchNorm2d(k))
-            model.append(nn.ReLU())
+        decoder_blocks = [512, 256, 128, 64, 32]
+        for k in decoder_blocks:
+            model.append(DecoderBlock(prev, k))
             prev = k
             size *= 2
 
-        # Final convolution to image
-        model.append(nn.Conv2d(prev, 3, kernel_size=3, padding=1))
-        model.append(nn.Sigmoid())  # or Tanh if you normalize images to [-1, 1]
+        model.append(nn.Conv2d(prev, 3, kernel_size=3, stride=2, padding=1))
+        model.append(nn.Sigmoid())  # usa Tanh se normalizzi in [-1,1]
 
         return model
 
     def forward(self, x, y):
         # skip_connections = []
-        h = x
-        for layer in self.encoder[:-3]:
-            h = layer(h)
-            # if isinstance(layer, ResidualBlock):
-            #     skip_connections.append(h)
-
-        for layer in self.encoder[-3:]:
+        
+        cond = y.unsqueeze(2).unsqueeze(3).expand(-1, -1, 64, 64)
+        h=torch.cat([x,cond], dim=1)         # l'outpur dell'encoder è 2*LATENT_SIZE
+        for layer in self.encoder[:]:
             h = layer(h)
 
         mu=h[:, :self.latent_size]    # da 0 a LATENT_SIZE corrisponde a mu
         log_sigma=h[:, self.latent_size:]     # da LATENT_SIZE fino alla fine corrisponde a log_sigma
         eps=torch.randn_like(mu)        # generazione del vettore latente secondo la normale
         z=eps*torch.exp(log_sigma)+mu       # generazione del vettore latente
-        out=torch.cat([z,y], dim=1)
+        y_proj = self.cond_latent_proj(y)
+        out=torch.cat([z,y_proj], dim=1)
 
-        # skip_idx = -1
-        # for i, layer in enumerate(self.decoder):
-            # if isinstance(layer, nn.Upsample):
-            #     out = layer(out)
-            #     if skip_idx >= -len(skip_connections):
-            #         skip = skip_connections[skip_idx]
-            #         skip = F.interpolate(skip, size=out.shape[2:])
-            #         out = torch.cat([out, skip], dim=1)
-            #         skip_idx -= 1
-            # else:
-                # out = layer(out)
         out = self.decoder(out)
 
         return out, mu, log_sigma
+    
+    
+    def generate_sample(self, y, save_folder, device='cpu'):
+        self.eval()
+        z = torch.randn(size=(self.latent_size,))
+        with torch.no_grad():
+            
+            y_proj = self.cond_latent_proj(y.to(device))
+            image_generated = self.decoder(torch.cat([z.unsqueeze(0).to(device),y_proj.unsqueeze(0).to(device)], dim=1))
+            
+        
+        label_str = '_'.join(str(int(v)) for v in y.tolist())
+        os.makedirs(save_folder, exist_ok=True)
+
+        img = (image_generated.squeeze().permute(1, 2, 0).cpu().numpy() * 255).astype('uint8')
+        img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(os.path.join(save_folder, f'generated_{label_str}.png'), img_bgr)
